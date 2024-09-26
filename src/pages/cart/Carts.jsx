c717888aa4579907a5c1ea4334ffbc6f936850cc
Roshan Chouhan <roshanchouhan29@gmail.com>
2024-09-26 23:16:16 +0530

    test

--- src/pages/cart/Carts.jsx
+++ src/pages/cart/Carts.jsx
@@ -27,7 +27,7 @@ const Carts = () => {
                       
                        <div className='flex justify-between items-center '> 
                          <p className='text-black text-xs'>Subtotal</p>
-                         <p className='text-black text-xs'>$356</p>
+                         <p className='text-black text-xs'>$350</p>
                       </div>
                       <div className='flex justify-between items-center '> 
                          <p className='text-black text-xs mt-2'>Shipping</p>
